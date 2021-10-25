9429717c89553b1ba3fba20dac9fe33987c8af60
Brandon-Gomez <salinasgomez1232@gmail.com>
2021-10-25 01:44:46 -0500

    commit

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
   </head>
   <body>
       
-<div class="container">
+
   <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-      <a class="navbar-brand" href="<?php echo base_url();?>"><img src="<?php echo base_url();?>" alt=""></a>
+      <a class="navbar-brand" href="index.php" alt="logo"> <img src="img/logo_transparente.png" width="150px" alt="logo"></a>
       <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
           aria-expanded="false" aria-label="Toggle navigation"></button>
       <div class="collapse navbar-collapse" id="collapsibleNavId">
@@ -22,7 +22,7 @@
                   <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
+                  <a class="nav-link" href="#">Mi cuenta</a>
               </li>
               <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Perifericos</a>
@@ -34,10 +34,9 @@
               </li>
           </ul>
           <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="text" placeholder="Buscar productos, marcas y más...">
+              <input class="form-control mr-sm-2" type="text" placeholder="Buscar productos, marcas...">
               <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
           </form>
-      </div>
   </nav>
 </div>
     
