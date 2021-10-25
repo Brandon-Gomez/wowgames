f28774bf3eb5310e4409c78b8634dd932a36a55e
Brandon-Gomez <salinasgomez1232@gmail.com>
2021-10-24 23:02:56 -0500

    logo

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
       
 <div class="container">
   <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <img src="css/" alt="">
       <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
           aria-expanded="false" aria-label="Toggle navigation"></button>
       <div class="collapse navbar-collapse" id="collapsibleNavId">
-          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
               <li class="nav-item active">
                   <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
               </li>
@@ -25,10 +25,11 @@
                   <a class="nav-link" href="#">Link</a>
               </li>
               <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                  <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Perifericos</a>
                   <div class="dropdown-menu" aria-labelledby="dropdownId">
-                      <a class="dropdown-item" href="#">Action 1</a>
-                      <a class="dropdown-item" href="#">Action 2</a>
+                      <a class="dropdown-item" href="#">Mouse</a>
+                      <a class="dropdown-item" href="#">Teclados</a>
+                      <a class="dropdown-item" href="#">Diademas</a>
                   </div>
               </li>
           </ul>
