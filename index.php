8f7a442be3b7b4d3a8df337c72265aeeea8f2f2a
Brandon-Gomez <salinasgomez1232@gmail.com>
2021-10-25 11:46:36 -0500

    commit

--- index.php
+++ index.php
@@ -10,15 +10,11 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/syles.css">
-<<<<<<< HEAD
     <link rel="stylesheet" href="">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js'></script>
     
-=======
-    <link rel="stylesheet" href="css/menuu.css">
->>>>>>> 50f2579dcb3758d01f69ac5a9b497da44648b97d
 </head>
   <body>
       
