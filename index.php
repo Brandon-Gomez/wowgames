cd0baab340da7d4716b393335a91599ac309e5c9
zackill5 <87101458+zackill5@users.noreply.github.com>
2021-10-26 00:37:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,14 @@
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js'></script>
     <link rel="stylesheet" href="css/menuu.css">
+    
 </head>
   <body>
+  <br>
+    <div class="alert alert-success">
+        Pantalla de mensaje.....
+        <a href="#" class="badge bg-success">Ver carrito</a>
+    </div>
       
 
   <nav class="navbar navbar-expand-sm navbar-dark bg-dark" height="4px">
@@ -44,7 +50,7 @@
                 
               <li class="nav-item">
                   
-                  <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                  <a class="nav-link" href="./carrito/carrito.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                     </svg>Carrito</a>
               </li>
@@ -115,6 +121,6 @@
 
         </ul>
     </div>
-    julian marica
+ 
   </body>
 </html>
