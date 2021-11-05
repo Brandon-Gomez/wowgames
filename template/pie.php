7c5b6c8fc66fdca570505bd1b080a34881d73092
Brandon-Gomez <salinasgomez1232@gmail.com>
2021-11-05 16:50:30 -0500

    footer

--- template/pie.php
+++ template/pie.php
@@ -1,3 +1,25 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<footer>
+        <div class="footer-content">
+          
+         <img src="img/logo_transparent.png" alt="" class="imgfoot">
+          <ul class="socials">
+            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          </ul>
+        </div>
+        <div class="footer-bottom">
+          <p>&copy; 2021 Wowgames. Reservados todos los derechos. Designed By <span>TPS 109</span></p>
+        </div>
+    </footer>
 </body>
 </html>
