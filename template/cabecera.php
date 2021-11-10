10739ff78ee34f09f1c2e0136a0406af5fbd3fa8
robinsamuel <robinortiz53@gmail.com>
2021-11-10 18:55:09 -0500

    menu

--- template/cabecera.php
+++ template/cabecera.php
@@ -1,19 +1,15 @@
 <!DOCTYPE html>
-<html lang="es">
-
+<html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>WowGames</title>
-	<link rel="stylesheet " href="styles.css">
-	<link rel="stylesheet" href="main.js">
-
-
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="styles.css">
+	<title>wongames</title>
 </head>
 <body>
 	<nav class="menu" id="menu">
-		<div class="contenedor contenedor-botones-menu ">
+		<div class="contenedor contenedor-botones-menu">
 			<button id="btn-menu-barras" class="btn-menu-barras"><i class="fas fa-bars"></i></button>
 			<button id="btn-menu-cerrar" class="btn-menu-cerrar"><i class="fas fa-times"></i></button>
 		</div>
@@ -21,102 +17,96 @@
 		<div class="contenedor contenedor-enlaces-nav">
 			<div class="btn-departamentos" id="btn-departamentos">
 				<p>Todos los<span>productos</span></p>
-				<i class=" fas fa-caret-down "></i>
+				<i class="fas fa-caret-down"></i>
 			</div>
 
 			<div class="enlaces">
-				<a href="# ">Promociones </a>
-				<a href="# ">Mesa de Regalos </a>
-				<a href="# ">Ayuda </a>
-				<a href="login.html">Cuenta </a>
+				<a href="login.html">Cuenta</a>
+				<a href="#">Promociones</a>
+				<a href="#">Mesa de Regalos</a>
+				<a href="#">Ayuda</a>
 			</div>
 		</div>
 
 		<div class="contenedor contenedor-grid">
-			<div class="grid activo" id="grid">
+			<div class="grid" id="grid">
 				<div class="categorias">
 					<button class="btn-regresar"><i class="fas fa-arrow-left"></i> Regresar</button>
 					<h3 class="subtitulo">Categorias</h3>
-					<a href="#" data-categoria="logitech"> Logitech <i class="fas fa-angle-right"></i></a>
-					<a href="#" data-categoria="hyperx">HyperX<i class="fas fa-angle-right"></i></a>
-					<a href="#" data-categoria="razer"> Razer <i class="fas fa-angle-right"></i></a>
-					<a href="#" data-categoria="redragon">Redragon <i class="fas fa-angle-right"></i></a>
-				</div>
-				
-				<div  class ="contenedor-subcategorias">
+
+					<a href="#" data-categoria="logitech">logitech<i class="fas fa-angle-right"></i></a>
+					<a href="#" data-categoria="hyperx">hyperx<i class="fas fa-angle-right"></i></a>
+					<a href="#" data-categoria="razer">razer<i class="fas fa-angle-right"></i></a>
+					<a href="#" data-categoria="redrago">redragon<i class="fas fa-angle-right"></i></a>
 					
-					<div class ="subcategoria activo"data-categoria ="logitech" >
-						<div class ="enlaces-subcategoria" >
-							<button class = "btn-regresar" ><i  class = "fas fa-arrow-left" ></i>Regresar</button >
-							<h3  class = "subtitulo" >Logitech</h3 >
-							<a href = "#" >mause</a>
-							<a href = "#" >teclado</a>
-							<a href = "#" >audifonos</a>
-							<a href = "#" >equipos de sonido</a>
-							<a href = "#" >Componentes</a>
+				</div>
+
+				<div class="contenedor-subcategorias">
+					<div class="subcategoria" data-categoria="logitech">
+						<div class="enlaces-subcategoria">
+							<button class="btn-regresar"><i class="fas fa-arrow-left"></i>Regresar</button>
+							<h3 class="subtitulo">logitech</h3>
+							<a href="#">mause</a>
+							<a href="#">Teclado</a>
+							<a href="#">audifonos eqipo de sonido</a>
+							<a href="#">equipo de sonido</a>
+							
 						</div>
 
-						<div  class ="banner-subcategoria" >
-							<a href = "#">
-								<img src = "img/logitech/1.png"  alt = "">
+						<div class="banner-subcategoria">
+							<a href="#">
+								<img src="img/logitech/1.png" alt="">
 							</a>
 						</div>
 
-						<div  class ="galeria-subcategoria" >
-							<a href = "#">
-								<img src = "img/logitech/2.png" alt = "">
+						<div class="galeria-subcategoria">
+							<a href="#">
+								<img src="img/logitech/2.png" alt="">
 							</a>
-							<a  href = "#">
-								<img src = "img/logitech/3.png " alt = "">
+							<a href="#">
+								<img src="img/logitech/3.png" alt="">
 							</a>
-							<a href = "#">
-								<img src = "img/logitech/4.png" alt = "">
+							<a href="#">
+								<img src="img/logitech/4.png" alt="">
 							</a>
-							<a href = "#">
-								<img src = "img/logitech/5.png" alt = "">
+							<a href="#">
+								<img src="img/logitech/5.png" alt="">
 							</a>
 						</div>
-					</div >
+					</div>
 
-				
-	                                   <!-- hyperx-->
+					<div class="subcategoria" data-categoria="hyperx">
+						<div class="enlaces-subcategoria">
+							<button class="btn-regresar"><i class="fas fa-arrow-left"></i>Regresar</button>
+							<h3 class="subtitulo">hyperx</h3>
+							<a href="#">mause</a>
+							<a href="#">teclado</a>
+							<a href="#">audifonos</a>
+							<a href="#">microfono</a>
+						</div>
 
-
-					<div  class = "subcategoria" data-categoria = "hyperx">
-						<div  class = "enlaces-subcategoria " >
-							<button  class = "btn-regresar" ><i  class = "fas fa-arrow-left "></i >Regresar</button >
-							<h3  class = "subtitulo " >hyperx</h3 >
-							<a href = " # " > mause </a >
-							<a href = " # " >teclado  </a >
-							<a href = " # " > adifonos </a >
-							<a href = " # " >  microfonos </a >
-						</div >
-
-						<div  class = " banner-subcategoria">
-							<a href = " # ">
-								<img  src = "img/hyperx/1.png " alt = "">
-							</a >
-						</div >
-
-						<div  class = " galeria-subcategoria " >
-							<a  href = " # " >
-								<img  src = "img/hyperx/2.png" alt = "">
+						<div class="banner-subcategoria">
+							<a href="#">
+								<img src="img/hyperx/1.png" alt="">
 							</a>
-							<a  href = " # " >
-								<img  src = "img/hyperx/3.png" alt = "">
-							</a>
-							<a href = " # " >
-								<img  src = "img/hyperx/4.png" alt = "">
-							</a>
-							<a  href = " # " >
-								<img  src = "img/hyperx/5.png" alt = "">
-							</a>
+						</div>
 
-						</div >
-					</div >
+						<div class="galeria-subcategoria">
+							<a href="#">
+								<img src="img/hyperx/2.png" alt="">
+							</a>
+							<a href="#">
+								<img src="img/hyperx/3.png" alt="">
+							</a>
+							<a href="#">
+								<img src="img/hyperx/4.png" alt="">
+							</a>
+							<a href="#">
+								<img src="img/hyperx/5.png" alt="">
+							</a>
+						</div>
+					</div>
 
-                                               <!--razer-->
-				
 					<div class="subcategoria" data-categoria="razer">
 						<div class="enlaces-subcategoria">
 							<button class="btn-regresar"><i class="fas fa-arrow-left"></i>Regresar</button>
@@ -124,7 +114,7 @@
 							<a href="#">mause</a>
 							<a href="#">teclado</a>
 							<a href="#">audifonos</a>
-							<a href="#">microfonos</a>
+							<a href="#">microfono</a>
 						</div>
 
 						<div class="banner-subcategoria">
@@ -149,7 +139,6 @@
 						</div>
 					</div>
 
-                                  <!--redrago-->
 
 					<div class="subcategoria" data-categoria="redrago">
 						<div class="enlaces-subcategoria">
@@ -182,12 +171,14 @@
 							</a>
 						</div>
 					</div>
-
-				</div>	
+				</div>
 			</div>
 		</div>
 	</nav>
-			
+
+				
+						
+					
 						
 
 	<main class="contenedor">
@@ -196,8 +187,5 @@
 	
 	<script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
 	<script src="main.js"></script>
-
 </body>
 </html>
-
-	
