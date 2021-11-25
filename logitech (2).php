fa3fa9b9f8ee73db592a5c4152fb0a6eb2a7ed69
Juan10-bit <juanbastidas080@gmail.com>
2021-11-25 00:19:55 -0500

    logi

--- logitech (2).php
+++ logitech (2).php
@@ -24,53 +24,10 @@
 </head>
 <body>
 
-	<nav class="navbar navbar-expand-sm navbar-dark bg-dark" height="4px">
-		<a class="navbar-brand" href="index.php" alt="logo"> <img src="../logitech/imagenes/logo_transparente.png" width="120px" alt="logo"></a>
-		<button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-			aria-expanded="false" aria-label="Toggle navigation"></button>
-		<div class="collapse navbar-collapse" id="collapsibleNavId">
-			<ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-			<form id="app" action="#">
-		  <label :data-state="state" for="search">
-		  <input type="text" placeholder="Buscar..." @click="state = 'opan'" @blur="state='close'"/>
-		  <i class='bx bx-search' @click="" aria-hidden="true" width="2px"></i>
-		  </label>
-	  </form><a style="font-size: 8pt; text-decoration: none" target="_blank" href="http://www.frontendfreecode.com"></a>      
-	  <script>var app = new Vue({
-		  el: '#app',
-		  data: { state: "close" } 
-	  });
-	  </script>   
-				<li class="nav-item">
-					<a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="19px" height="19px" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-					  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-					  </svg>Mi cuenta</a>
-				  </li>
-				  
-				<li class="nav-item">
-					
-					<a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
-					  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-					  </svg>Carrito</a>
-				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Productos</a>
-					<div class="dropdown-menu" aria-labelledby="dropdownId">
-						<a class="dropdown-item" href="#">Mouse</a>
-						<a class="dropdown-item" href="#">Teclados</a>
-						<a class="dropdown-item" href="#">Diademas</a>
-					</div>
-				</li>
-			</ul>                                     
-	</nav>
-
+<?php include("template/cabecera.php")?>
 	
 
-	<div class="banner">
-		<div style="color: white;" class="info">
-			<h1 style="text-align: center;">BIENVENIDOS A LOGITECH</h1>
-			<p >Ver online ahora! Mira la mayor selección de productos Logitech. Tienda online oficial con ofertas exclusivas.</p>		
-		</div>
+<div class="banner">
 	</div>
 	   <div class="nav">
 		<h3 style="text-align: center;">DESCUBRÍ NUESTRO UNIVERSO DE PRODUCTOS RECOMENDADOS PARA VOS</h3>
@@ -79,25 +36,26 @@
     
 	   <div class="content">
 	   <div class="productos">
-		<h3>MK200 Media Corded Keyboard and Mouse Combo</h3>
-		<img src="../logitech/imagenes/pro7.jpg">
-		<h3>Combinación USB Plug-and-Play con teclas multimedia.</h3><br>
+		<h3>TECLADO PRO</h3><br><br>
+		<img src="../logitech/imagenes/nuevo.jpg"><br><br><br><br><br>
+		<h3>Edición League of Legends</h3><br>
 		<button class="button-add">AGREGAR AL CARRITO</button>
+
 	</div> 
 
 		
 		<div class="productos">
-		<h3>MX KEYS</h3>
-		<img src="../logitech/imagenes/pro2.jpg">
-		<h3>Teclado inalámbrico iluminado avanzado.</h3><br><br>
+		<h3>G213 PRODIGY</h3><br><br>
+		<img src="../logitech/imagenes/nuevo2.jpg"><br><br><br><br>
+		<h3>Teclado para juegos RGB</h3><br><br>
 		<button class="button-add">AGREGAR AL CARRITO</button>
 		</div>
 
 		
 		<div class="productos">
-        <h3>ERGO K860</h3>
-		<img src="../logitech/imagenes/pro3.jpg">
-		<h3>Teclado ergonómico partido</h3><br><br><br>
+        <h3>G513 CARBON</h3><br><br>
+		<img src="../logitech/imagenes/nuevo3.jpg"><br><br><br><br>
+		<h3>TECLADO MECÁNICO RGB PARA JUEGOS</h3><br>
 		<button class="button-add">AGREGAR AL CARRITO</button>
 		</div><br>
 
@@ -105,7 +63,7 @@
 
 		<div class="productos">
 			<h3>MK345 Comfort Wireless Keyboard and Mouse Combo</h3><br>
-			<img src="../logitech/imagenes/pro6.jpg"><br><br>
+			<img src="../logitech/imagenes/pro6.jpg"><br><br><br>
 			<h3>Cómoda combinación inalámbrica con reposamanos.</h3><br>
 			<button class="button-add">AGREGAR AL CARRITO</button>
 		</div>
@@ -122,9 +80,9 @@
 
 	   <div class="content">
 		<div class="productos">
-        <h3>MX ANIWHERE 3</h3>
-		<img src="../logitech/imagenes/pro1.jpg"><br><br>
-		<h1>Mouse compacto de alto desempeño.</h3><br><br>
+        <h3>G502</h3><br><br>
+		<img src="../logitech/imagenes/pro.jpg"><br><br><br><br>
+		<h1>Mouse inalámbrico para juego LIGHTSPEED</h3><br>
 		<button class="button-add">AGREGAR AL CARRITO</button>
 		</div>
 
@@ -155,28 +113,86 @@
 
 	</div>
 
-	   
+	<div class="nav">
+		<h3 style="text-align: center;">EQUIPOS DE SONIDO</h3>
+	</div>
+	  	
+	    <div class="content">
+		<div class="productos">
+        <h3>Altavoces estéreo S120</h3><br>
+		<img src="../logitech/imagenes/equi1.jpg"><br><br>
+		<h3>Sonido estéreo nítido</h3><br><br>
+		<button class="button-add">AGREGAR AL CARRITO</button>
+		</div>
 
-	 <footer>
-        <div class="footer-content">
-          
-         <img src="img/wowgames_logo.svg" alt="" class="imgfoot">
-          <ul class="socials">
-            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-          </ul>
-        </div>
-        <div class="footer-bottom">
-          <p>&copy; 2021 Wowgames. Reservados todos los derechos. Designed By <span>TPS 109</span></p>
-        </div>
-    </footer>
+		<div class="productos">
+        <h3>Z407</h3>
+		<img src="../logitech/imagenes/equi2.jpg"><br><br>
+		<h3>Altavoces Bluetooth para computadora con subwoofer y control inalámbrico</h3><br><br>
+		<button class="button-add">AGREGAR AL CARRITO</button>
+		</div>
+
+
+		<div class="productos">
+        <h3>Altavoces estéreo Z150</h3>
+		<img src="../logitech/imagenes/equi3.jpg"><br><br><br>
+		<h3>Sonido estéreo nítido</h3><br><br>
+		<button class="button-add">AGREGAR AL CARRITO</button>
+		</div>
+
+
+		<div class="productos">
+        <h3>Z607 5.1 Surround Sound Speaker System</h3>
+		<img src="../logitech/imagenes/equi4.jpg"><br><br>
+		<h3>Sonido potente con Bluetooth</h3><br><br>
+		<button class="button-add">AGREGAR AL CARRITO</button>
+		</div>
+
+	</div>
+
+	<div class="nav">
+		<h3 style="text-align: center;">AUDIFONOS</h3>
+	</div>
+	  	
+	    <div class="content">
+		<div class="productos">
+        <h3>G935</h3><br><br><br>
+		<img src="../logitech/imagenes/audi1.jpg"><br><br>
+		<h3>Audífonos inalámbricos con micrófono, sonido envolvente 7.1 y LIGHTSYNC para juegos</h3><br><br>
+		<button class="button-add">AGREGAR AL CARRITO</button>
+		</div>
+
+		<div class="productos">
+        <h3>G733</h3><br><br>
+		<img src="../logitech/imagenes/audi2.jpg"><br><br><br>
+		<h3>Audífonos con micrófono inalámbricos LIGHTSPEED RGB para juegos</h3><br><br>
+		<button class="button-add">AGREGAR AL CARRITO</button>
+		</div>
+
+
+		<div class="productos">
+        <h3>AUDÍFONOS CON MICRÓFONO PRO X PARA JUEGOS</h3><br><br>
+		<img src="../logitech/imagenes/audi3.jpg"><br><br><br>
+		<h3>Edición League of Legends</h3><br><br>
+		<button class="button-add">AGREGAR AL CARRITO</button>
+		</div>
+
+
+		<div class="productos">
+        <h3>G335</h3><br><br><br>
+		<img src="../logitech/imagenes/audi4.jpg"><br><br>
+		<h3>Audífonos con cable y micrófono para juegos</h3><br><br>
+		<button class="button-add">AGREGAR AL CARRITO</button>
+		</div>
+
+	</div>
+
+
+	   
+	<?php include("template/pie.php")?>
+	
 
 </body>
-<script src="https://kit.fontawesome.com/d0dc7f6b6a.js" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
 
 	  
 </body>
