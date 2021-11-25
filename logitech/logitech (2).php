00c02f2037af6effd39ad5d8bb83e7b83b3e38ee
Brandon-Gomez <salinasgomez1232@gmail.com>
2021-11-25 11:42:17 -0500

    Update logitech (2).php

--- logitech/logitech (2).php
+++ logitech/logitech (2).php
@@ -17,7 +17,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js'></script>
-    <link rel="stylesheet" href="css/menuu.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 </head>
