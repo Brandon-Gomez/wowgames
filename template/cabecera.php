ec56358ef8d25948d85d48dec49291662483966a
Brandon-Gomez <salinasgomez1232@gmail.com>
2021-12-01 17:35:07 -0500

    Revert "bastidas perro hijueputa"
    This reverts commit f3542ed7ba2f27e1fe59182d55255bdc1dbfded0.

--- template/cabecera.php
+++ template/cabecera.php
@@ -5,12 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="css/nav.css">
-<<<<<<< HEAD
 	<title>wowgames</title>
-=======
-
-	<title>Wowgames</title>
->>>>>>> parent of a677f55 (Merge branch 'main' into Bastidas)
 </head>
 <body>
 	<nav class="menu" id="menu">
