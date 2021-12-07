d8296d2472cd94fe2a4d13b5bb6c965eb07dc235
zackill5 <87101458+zackill5@users.noreply.github.com>
2021-12-07 10:07:01 -0500

    Update login.php

--- login.php
+++ login.php
@@ -4,21 +4,21 @@ session_start();
 
 require 'conexion.php';
 
-if (!empty($_POST['email']) && !empty($_POST['password'])) {
-  $records = $conn->prepare('SELECT id,email,password FROM users WHERE email=:email');
-  $records->bindParam(':email', $_POST['email']);
-  $records->execute();
-  $results = $records->fetch(PDO::FETCH_ASSOC);
+// if (!empty($_POST['email']) && !empty($_POST['password'])) {
+//   $records = $conn->prepare('SELECT id,email,password FROM users WHERE email=:email');
+//   $records->bindParam(':email', $_POST['email']);
+//   $records->execute();
+//   $results = $records->fetch(PDO::FETCH_ASSOC);
 
-  $message ='';
+//   $message ='';
 
-if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
-  $_SESSION['user_id']=$results['id'];
-  header('Location:index.php');
-  }else{
-    $message = 'Sorry, those credentials do not match';
-  }
-}
+// if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
+//   $_SESSION['user_id']=$results['id'];
+//   header('Location:index.php');
+//   }else{
+  //   $message = 'Sorry, those credentials do not match';
+  // }
+// }
 
 
 ?>
