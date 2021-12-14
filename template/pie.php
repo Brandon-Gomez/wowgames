0f41559313234dbbeb9798792db724cf7a376990
Brandon-Gomez <salinasgomez1232@gmail.com>
2021-12-14 01:11:26 -0500

    FOOTER

--- template/pie.php
+++ template/pie.php
@@ -6,17 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/">
+    <link rel="stylesheet" href="css/nav.css">
 </head>
 <body>
 <footer>
         <div class="footer-content">
+         <img src="img/wowgames_logo.svg" alt="" class="imgfoot" > 
           
           <ul class="socials">
+
             <li><a href="#"><i class="fab fa-facebook"></i></a></li>
             <li><a href="#"><i class="fa fa-instagram"></i></a></li>
             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-         <img src="../img/wowgames_logo.svg" alt="" class="imgfoot" > 
 
           </ul>
         </div>
